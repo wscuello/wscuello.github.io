46aba2fce4345c78b055cb168ca2b0419dd8dd14
schang1 <schang1@godaddy.com>
2021-05-12 21:01:59 -0400

    added favicon and minor clenaup

--- scripts/header.js
+++ scripts/header.js
@@ -4,13 +4,3 @@ this is a header function that makes it so I don't have to copy header.html into
 $(function(){
   $("#header").load("header.html");
 })
-
-
-
-$(document).ready(function() {
-  $("[href]").each(function() {
-    if (this.href == window.location.href) {
-      $(this).addClass("current");
-    }
-  });
-});
